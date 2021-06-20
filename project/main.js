f79d7695c97c80887a10086578af32572b52772b
Tslilb <tslil.b8@gmail.com>
2021-06-20 10:29:33 +0300

    done

--- project/main.js
+++ project/main.js
@@ -1,14 +1,11 @@
 require('dotenv').config();
 const twitter_api = require("./twitter_module/twitter_api");
 const health = require("./health_module/health");
-var express = require("express");
-var app = express();
-var router = express.Router();
+const express = require("express");
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || "80";
 
-
-var port = process.env.PORT || "80";
-
-app.use(express.static('client'));
 
 const rout = router.get("/", (req, res) => res.send("Welcome to Tslil's app"));
 app.use(rout);
@@ -17,21 +14,16 @@ app.use(rout);
 // Routings
 app.use("/health", health);
 
-
-
 app.get("/tweets", async function (req, res) {
-
   const query = req.query.query;
   const tweets = await twitter_api.query(query);
   res.json(tweets);
-
 })
 
 app.use(function (err, req, res, next) {
   console.error(err);
   res.status(err.status || 500).send(err.message);
 });
-
 
 const server = app.listen(port, () => {
   console.log(`Server listen on port ${port}`);
